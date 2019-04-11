5ac1e0456c564cfe206c8aa87f6fd3cd7cc63d13
= <=>
2019-04-11 13:42:54 +0530

    added form and imgae uplaod code

---
+++ developerblog/users/forms.py
@@ -0,0 +1,46 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo
+from wtforms import ValidationError
+from flask_wtf import FileField, FileAllowed
+
+from flask_login import current_user
+from developerblog.models import User
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password  = PasswordField('Password', validators[DataRequired()])
+    submit   = SubmitField('LogIn')
+
+
+class RegistrationForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(), EqualTo('pass_confirm',message='Password Must Match!')])
+    pass_confirm = PasswordField('Confirm Password', validators=[DataRequired()])
+
+    submit = SubmitField('Register')
+
+
+    def check_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('Your email is already Registered!')
+
+    def check_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('Username match Please Try Another One !')
+
+
+class UpdateUserForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    username = StringField('Username', validators=[DataRequired()])
+    profile = FileField('Upload Profile', validators[FileAllowed(['jpeg','png','jpg'])])
+    submit = SubmitField('Update')
+
+    def check_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('Your email is already Registered!')
+
+    def check_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('Username match Please Try Another One !')
