1fa716be16787f008c797101d8de57c78f6c6174
= <=>
2019-04-11 13:01:46 +0530

    model set for users and blog

--- developerblog/__init__.py
+++ developerblog/__init__.py
@@ -1,6 +1,38 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app  = Flask(__name__)
+
+######################################
+##########  DATABASE #################
+######################################
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:1234@localhost/flaskblog"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+#connecting application to the database
+Migrate(app,db)
+
+
+
+#########################################
+#### Login config
+
+login_manager = LoginManager()
+
+login_manager.init_app(app)
+login_manager.login_view = 'users.login'
+
+
+
+#########################################
+
+
+
 
 from developerblog.core.views import core
 app.register_blueprint(core)
